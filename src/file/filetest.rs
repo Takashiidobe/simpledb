eee9b963ac88ea29538a4b89349ba95847ffb3d1
Takashi Idobe <mail@takashiidobe.com>
2023-11-20 09:36:51 -0500

    Chapter 3: Disk and File Management

---
+++ src/file/filetest.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use std::fs;
+
+    #[test]
+    fn filetest() {
+        let db = SimpleDB::new("filetest", 400, 8);
+        let fm = db.file_mgr();
+
+        let blk = BlockId::new("testfile", 2);
+        let mut p1 = Page::new(fm.block_size());
+        let pos1 = 88;
+        p1.set_string(pos1, "abcdefghijklm");
+        let size = Page::max_length("abcdefghijklm".len());
+        let pos2 = pos1 + size;
+        p1.set_int(pos2, 345);
+        fm.write(&blk, &mut p1).unwrap();
+
+        let mut p2 = Page::new(fm.block_size());
+        fm.read(&blk, &mut p2).unwrap();
+
+        fs::remove_dir_all("filetest").unwrap();
+
+        assert_eq!(105, pos2);
+        assert_eq!(345, p2.get_int(pos2));
+        assert_eq!(88, pos1);
+        assert_eq!("abcdefghijklm", p2.get_string(pos1).unwrap());
+    }
+}
